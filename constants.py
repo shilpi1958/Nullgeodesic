fcf1768f5997663779ae3ae231003ee121417aa7
shilpi1958 <shilpi1958@gmail.com>
2019-07-17 13:42:36 +0530

    add

--- constants.py
+++ constants.py
@@ -1,73 +1,36 @@
 import numpy as np
-from astropy import constants
+import astropy.units as u
 
+from einsteinpy.utils import kerr_utils
 from einsteinpy.coordinates import BoyerLindquistDifferential
-from odessay_initialcondition import initial_velocity_photon , coord_photon
+from initialcondition import InitialConditionsBHFrame
 
-class MotionConstants:
+class MotionConstants(InitialConditionsBHFrame):
 
     """
     Class for defining Kerr Motion constant
     """
 
-    @u.quantity_input(M=u.kg,
-            r_obs =u.km,
-            theta_obs=u.rad,
-            phi_obs=u.rad ,
-            a=u.km)
+    @u.quantity_input(
+        M=u.kg,
+        r_obs =u.km,
+        theta_obs=u.rad,
+        phi_obs=u.rad ,
+        a=u.km,
+        x = u.km,
+        y = u.km,
+        z = u.km)
 
-    def __init__(self, r_obs ,theta_obs, phi_obs,  a ):
-        self.a = a
+    def __init__(self, r_obs ,theta_obs, phi_obs, a, M, x, y ,z ):
+
+        super(MotionConstants, self).__init__(r_obs ,theta_obs, phi_obs, a)
         self.M = M
-        self.r_obs= r_obs
-        self.theta_obs= theta_obs
-        self.phi_obs= r_obs
+        self.x = x
+        self.y = y
+        self.z = z
 
 
-    def sigma(r, theta, a):
-        """
-        Returns the value r^2 + a^2 * cos^2(theta)
-        Specific to Boyer-Lindquist coordinates
-        Parameters
-        ----------
-        r : float
-            Component r in vector
-        theta : float
-            Component theta in vector
-        a : float
-            Spin factor
-        Returns
-        -------
-        float
-            The value r^2 + a^2 * cos^2(theta)
-
-        """
-        return (r ** 2) + ((a * np.cos(theta)) ** 2)
-
-
-
-    def delta(r, a , M):
-        """
-        Returns the value r^2 - 2 * r + a^2
-        Specific to Boyer-Lindquist coordinates
-        Parameters
-        ----------
-        r : float
-            Component r in vector
-        M : float
-            Mass of massive body
-        a : float
-            Spin factor
-        Returns
-        -------
-        float
-            The value r^2 - 2 * r * M+ a^2
-
-        """
-        return (r ** 2) - (2 * r * M) + (a ** 2)
-
-
-    def energy(self, x , y , z):
+    def energy(self):
 
         """
         Returns Energy (E) which is constant along the geodesic.
@@ -102,15 +65,19 @@ class MotionConstants:
 
         """
 
-        coord = coord_photon(self)
-        r = coord[0]
-        theta = coord[1]
-        sg, dl ,AA = sigma(r, theta, a), delta(r, M, a), AA(r, theta, a)
-        v_ini = initial_velocity_photon(self)
+        coordinates = self.coord_photon( self.x ,self.y ,self.z)
+        coords = coordinates.si_values()
+        r        = coords[0] * u.km
+        theta    = coords[1] * u.rad
+        sg       = kerr_utils.sigma(r, theta, self.a)
+        dl       = kerr_utils.delta(r , self.M , self.a)
+        v_ini    = self.initial_velocity_photon(self.x ,self.y ,self.z)
+        restmass = 0
 
-        return (((sg - (2 * r))/(sg * dl))((sg * v_ini[0] * v_ini[0]) +
-                (sg * dl * v_ini[1] * v_ini[1])) - (dl * restmass))
-                + (dl * (v_ini[2] * np.cos(theta)) ** 2 )
+        A = (sg - (2 * r))/(sg * dl)
+        B = (sg * v_ini[0] * v_ini[0]) + (sg * dl * v_ini[1] * v_ini[1]) - (dl * restmass)
+
+        return np.sqrt((A * B) -(dl * (v_ini[2] * np.cos(theta))** 2))
 
 
     def angular_momentum_z(self):
@@ -119,30 +86,39 @@ class MotionConstants:
         Returns Angular momentum (Lz) which is constant along the geodesic.
         """
 
-        coord = coord_photon(self)
-        r = coord[0]
-        theta = coord[1]
-        sg, dl ,AA = sigma(r, theta, a), delta(r, M, a), AA(r, theta, a)
-        v_ini = initial_velocity_photon(self)
-        E = energy(self)
+        coordinates = self.coord_photon( self.x ,self.y ,self.z)
+        coords = coordinates.si_values()
+        r        = coords[0] * u.km
+        theta    = coords[1] * u.rad
+        sg       = kerr_utils.sigma(r, theta, self.a)
+        dl       = kerr_utils.delta(r , self.M , self.a, c=constant.c.value, G=constant.G.value)
+        v_ini    = self.initial_velocity_photon( self.x ,self.y ,self.z)
+        restmass = 0
+        E = self.energy()
 
-        return (((dl * sg * v_ini[2]) - (2 * a * r * E)) * ((np.sin(theta)) ** 2))
-                / (sg - (2 * r))
+        A = (dl * sg * v_ini[2]) - (2 * a * r * E)
+        B = sg - (2 * r)
+
+        return (A  * np.sin(theta) *np.sin(theta)) / B
 
 
-    def carter_const():
+    def carter_const(self):
 
         """
         Returns Carter Constant (Q) which is constant along the geodesic.
         """
-        lz = angular_momentum_z(self)
-        E = energy(self)
-        v_ini = initial_velocity_photon(self)
+        lz = self.angular_momentum_z()
+        E = self.energy()
+        v_ini    = self.initial_velocity_photon( self.x ,self.y ,self.z)
         p_theta =  v_ini[1] * E
+        restmass = 0
 
-        coord = coord_photon(self)
-        theta = coord[1]
+        coordinates = self.coord_photon( self.x ,self.y ,self.z)
+        coords = coordinates.si_values()
+        theta    = coords[1] * u.rad
 
-        return ((p_theta) ** 2) +
-                (((lz * np.cosec(theta)) ** 2)
-                    - ((a * a) 8 (E * E + restmass))) * (np.cos(theta) * np.cos(theta))
+        A = lz * np.cosec(theta)
+        B = (a * a) * (E * E + restmass)
+
+
+        return (p_theta * p_theta) + (A - B) * (np.cos(theta) * np.cos(theta))
